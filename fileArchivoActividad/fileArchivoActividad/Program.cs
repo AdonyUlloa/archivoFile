86759bb9070d4fcd5efcd0d42d11ea662e4e0e2f
Adony Ulloa <adonyulloa@gmail.com>
2021-08-28 11:15:31 -0600

    ultimos detalles de mi parte

--- fileArchivoActividad/fileArchivoActividad/Program.cs
+++ fileArchivoActividad/fileArchivoActividad/Program.cs
@@ -18,5 +18,96 @@ namespace fileArchivoActividad
                 showMenu = Menu();
             }
         }
+        private static bool Menu()
+        {
+
+            Console.WriteLine("REGISTRO CLINICO");
+            Console.WriteLine("Selecionar operacion a realizar: ");
+            Console.WriteLine("1. Registrar nuevo paciete: ");
+            Console.WriteLine("2. Actualizar dato de paciente: ");
+            Console.WriteLine("3. Eliminar dato de un paciente: ");
+            Console.WriteLine("4. Mostrar listado de pacientes: ");
+            Console.WriteLine("5. Salir: ");
+            Console.Write("\nOpcion: ");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    registerPaciente();
+                    return true;
+                case "2":
+                    return true;
+                case "3":
+                    deleteData();
+                    Console.ReadKey();
+                    return true;
+                case "4":
+                    Console.WriteLine("LISTADO DE ESTUDIATES");
+                    foreach (KeyValuePair<object, object> data in readFile())
+                    {
+                        Console.WriteLine("{0}: {1}", data.Key, data.Value);
+                    }
+                    Console.ReadKey();
+                    return true;
+                case "5":
+                    return false;
+                default:
+                    return false;
+
+            }
+        }
+
+        private static string getPath()
+        {
+            string path = @"C:\Ejemplo\registro.txt";
+            return path;
+        }
+
+        private static void registerPaciente()
+        {
+            Console.WriteLine("DATOS DEL PACIENTE");
+            Console.Write("Nombre completo: ");
+            string fullname = Console.ReadLine();
+            Console.Write("Edad: ");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            using (StreamWriter sw = File.AppendText(getPath()))
+            {
+                sw.WriteLine("{0}; {1}", fullname, age);
+                sw.Close();
+            }
+        }
+
+        private static Dictionary<object, object> readFile()
+        {
+            Dictionary<object, object> listData = new Dictionary<object, object>();
+
+            using (var reader = new StreamReader(getPath()))
+            {
+                string lines;
+
+                while ((lines = reader.ReadLine()) != null)
+                {
+                    string[] keyvalue = lines.Split(';');
+                    if (keyvalue.Length == 2)
+                    {
+                        listData.Add(keyvalue[0], keyvalue[1]);
+                    }
+                }
+            }
+            return listData;
+
+        }
+
+        private static bool search(string name)
+        {
+            if (!readFile().ContainsKey(name))
+            {
+                return false;
+            }
+            return true;
+        }
+       
+
     }
 }
