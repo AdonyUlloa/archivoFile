0e12cf5d18e1190f385f6fb4d5fa9559e030cb18
Adony Ulloa <adonyulloa@gmail.com>
2021-08-28 10:55:59 -0600

    segundo commit

---
+++ fileArchivoActividad/fileArchivoActividad/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace fileArchivoActividad
+{
+    class archivoClinico
+    {
+        static void Main()
+        {
+            bool showMenu = true;
+
+            while (showMenu)
+            {
+                showMenu = Menu();
+            }
+        }
+    }
+}
